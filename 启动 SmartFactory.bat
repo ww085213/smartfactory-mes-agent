95c243d7472a693a15fea295d6679e96e25b223a
Memory- <285351117+ww085213@users.noreply.github.com>
2026-09-03 00:40:59 +0800

    fix: make Windows launcher compatible with cmd

--- 启动 SmartFactory.bat
+++ 启动 SmartFactory.bat
@@ -13,6 +13,6 @@ if not exist "backend\node_modules" (
   )
 )
 
-start "SmartFactory MES 服务" cmd /k "cd /d \"%~dp0\" && npm run dev"
+start "SmartFactory MES 服务" cmd /k "cd /d ""%~dp0"" && npm run dev"
 timeout /t 5 /nobreak >nul
 start "" "http://localhost:5173"
